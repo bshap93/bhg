c4e8c5603c482d1c493de091434d14f3e41544ed
Ben Shapiro <bshap93@gmail.com>
2024-01-12 19:44:09 -0700

    Added listening logic

--- netkitty/netkitty.py
+++ netkitty/netkitty.py
@@ -94,5 +94,36 @@ class NetKitty:
 
     def handle(self, client_socket):
         if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
+
+
+        elif self.args.upload:
+            file_buffer = b''
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                else:
+                    break
+
+            with open(self.args.upload, 'wb') as f:
+                f.write(file_buffer)
+            message = f'Saved file {self.args.upload}'
+            client_socket.send(message.encode())
+
+        elif self.args.command:
+            cmd_buffer = b''
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                     file_buffer += data
+                else:
+                    break
+
+            with open(self.args.upload, 'wb') as f:
+                f.write(file_buffer)
+            message = f'Saved file {self.args.upload}'
+            client_socket.send(message.encode())
 
 
