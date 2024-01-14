98033692d7e866727f6192734349a065d027d4e5
Ben Shapiro <bshap93@gmail.com>
2024-01-13 18:35:18 -0700

    fixed errors

--- netkitty/netkitty.py
+++ netkitty/netkitty.py
@@ -1,5 +1,4 @@
 import argparse
-from os import access
 import socket
 import shlex
 import subprocess
@@ -15,39 +14,10 @@ def execute(cmd):
                                      stderr=subprocess.STDOUT)
     return output.decode()
 
-if if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description='BHP' Netkitty Tool',
-        formatter_class=argparse.argparse.RawDescriptionHelpFormatter,
-        epilog=textwrap.dedent('''Example:
-
-            netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
-            netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
-            netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
-            echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
-            netcat.py -t 192.168.1.108 -p 5555 # connect to server
-        '''))
-    parser.add_argument('-c', '--command', action='store_true', help='command shell')
-    parser.add_argument('-c', '--command', action='store_true', help='command shell') 
-    parser.add_argument('-e', '--execute', help='execute specified command')
-    parser.add_argument('-l', '--listen', action='store_true', help='listen')
-    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
-    parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
-    parser.add_argument('-u', '--upload', help='upload file')
-    args = parser.parse_args()
-    if args.listen: 
-        buffer = ''
-    else:
-        buffer = sys.stdin.read()
-
-    nk = NetKitty(args, buffer.encode())
-    nk.run()
-
-
 class NetKitty:
     def __init__(self, args, buffer=None):
         self.args = args
-        self.buffer = args
+        self.buffer = buffer 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -95,8 +65,9 @@ class NetKitty:
     def handle(self, client_socket):
         if self.args.execute:
             output = execute(self.args.execute)
+            if output is None:
+                return 
             client_socket.send(output.encode())
-
 
         elif self.args.upload:
             file_buffer = b''
@@ -115,15 +86,47 @@ class NetKitty:
         elif self.args.command:
             cmd_buffer = b''
             while True:
-                data = client_socket.recv(4096)
-                if data:
-                     file_buffer += data
-                else:
-                    break
+                try:
+                    client_socket.send(b'BHP: #> ')
+                    while '\n' not in cmd_buffer.decode():
+                        cmd_buffer += client_socket.recv(64)
+                    response = execute(cmd_buffer.decode())
+                    if response:
+                        client_socket.send(response.encode())
+                    cmd_buffer = b''
+                except Exception as e:
+                    print(f'server killed {e}')
+                    self.socket.close()
+                    sys.exit()
 
-            with open(self.args.upload, 'wb') as f:
-                f.write(file_buffer)
-            message = f'Saved file {self.args.upload}'
-            client_socket.send(message.encode())
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='BHP Netkitty Tool',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent('''Example:
+
+            netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
+            netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
+            netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
+            echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
+            netcat.py -t 192.168.1.108 -p 5555 # connect to server
+        '''))
+    parser.add_argument('-c', '--command', action='store_true', help='command shell') 
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('-l', '--listen', action='store_true', help='listen')
+    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
+    parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
+    parser.add_argument('-u', '--upload', help='upload file')
+    args = parser.parse_args()
+    if args.listen: 
+        buffer = ''
+    else:
+        buffer = sys.stdin.read()
+
+    nk = NetKitty(args, buffer.encode())
+    nk.run()
 
 
